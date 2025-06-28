e7ed3bdeebec7e0b1fc922f964f5504d3be62b9f
Abisheck <ramaniabira123@gmail.com>
2025-06-28 19:36:31 +0530

    new files added

---
+++ practice.py
@@ -0,0 +1,26 @@
+import random
+import sys
+
+# a = "     Abisheck Maha    "
+# print(a.upper())
+# print(a.strip())
+# print(a.replace("k", "K"))
+# for x in a:
+#     print (len(x))
+
+# age = 21
+# details = f"hi,this is abisheck and my age is {age}"#just simply we can put f infront of our str and we can use our int value inside our str
+# print(details)
+
+# list = ["abi","anandh"]
+
+# list.insert(2,"Abimaha")
+# list.append("ramani")
+# print(list)
+
+#to append to two list
+list1 = ["Abi","Dhiraj","Amma"]
+list2 = ["bala","maha","lakshmi"]
+list1.extend(list2)
+list1.remove("Dhiraj")
+print(list1)
